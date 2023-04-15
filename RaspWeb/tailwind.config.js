1b7a4702d45ba0be0e1c745e4fef75dfb37a0c43
Alfonz101 <73038744+Alfonz101@users.noreply.github.com>
2023-04-15 23:47:14 +0200

    Uploaded from laptop

---
+++ RaspWeb/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./build/*.{html, js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto']
+      }
+  },
+  },
+  plugins: [],
+  
+}
+
